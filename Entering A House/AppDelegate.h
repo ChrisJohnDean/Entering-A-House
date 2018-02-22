878bdc019aa191555ef92a840af3fe4f3d8bd1c6
Chris Dean <chrisdean@Chris-Deans-MacBook-Pro.local>
2018-02-21 18:01:46 -0800

    Initial Commit

---
+++ Entering A House/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Entering A House
+//
+//  Created by Chris Dean on 2018-02-21.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Entering A House/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Entering A House
+//
+//  Created by Chris Dean on 2018-02-21.
+//  Copyright © 2018 Chris Dean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
